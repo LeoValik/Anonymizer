042834df939f4c20aebf32c77dc90f7d552ee6ea
LeoValik <troval12989@gmail.com>
2019-05-16 22:24:03 +0300

    Anonymizer Begin

---
+++ index.js
@@ -0,0 +1,18 @@
+const skypeBtn = document.getElementById("skypeBtn");
+function someFunc(){
+    let skypeValue = document.getElementById("skypeValue").value,
+    skypeResult = document.getElementById("skypeResult");
+
+    //alert(document.getElementById("skypeValue").value);
+    skypeResult.innerHTML = skypeValue;
+    }
+skypeBtn.onclick = someFunc;
+    
+
+//  Skype usernames anonymizer
+function skypeAnonymizer() {
+    let skypeValue = document.getElementById("skypeValue").value,
+    skypeResult = document.getElementById("skypeResult");
+
+    
+}
