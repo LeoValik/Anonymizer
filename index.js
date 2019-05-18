32b14f720f7a02c011deff15f69e31626842edfc
LeoValik <troval12989@gmail.com>
2019-05-18 19:04:49 +0300

    Skype: 3 of 4 tests is done

--- index.js
+++ index.js
@@ -1,93 +1,56 @@
 const skypeBtn = document.getElementById("skypeBtn");
-/*function someFunc(){
-    let skypeValue = document.getElementById("skypeValue").value,
-    skypeResult = document.getElementById("skypeResult");
-
-    //alert(document.getElementById("skypeValue").value);
-    skypeResult.innerHTML = skypeValue;
-    }
-*/
-//skypeBtn.onclick = someFunc;
-    
+const phoneBtn = document.getElementById("phoneBtn");
 
 //  Skype usernames anonymizer
 function skypeAnonymizer() {
     let skypeValue = document.getElementById("skypeValue").value,
     skypeResult = document.getElementById("skypeResult");
 
-
-    //let spliter = skypeValue.split(/:|,/);
     let spliter = skypeValue.match(/\w+/g);
-    console.log(skypeValue);
+    console.log('Skype input is ' + skypeValue);
     console.log(spliter);
-/*
+
+    let resultSkype;
     for (let i=0; i < spliter.length; i++) {
         if (spliter[i] == 'skype')
             {
                 if (spliter[i+2] == 'call') 
                 {
-                    spliter[i+1] = 'X';
+                    //spliter[i+1] = 'X';
+                    resultSkype = skypeValue.split(`${spliter[i+1]}`).join('X');
+                    console.log(skypeValue.split(`${spliter[i+1]}`).join('X'));
                 }
                 else if (spliter[i+2] == 'chat') 
                 {
-                    spliter[i+1] = '*';
+                    //spliter[i+1] = '*';
+                    resultSkype = skypeValue.split(`${spliter[i+1]}`).join('*');
+                    console.log(skypeValue.split(`${spliter[i+1]}`).join('*'));
                 }
-                else spliter[i+1] = '#';
-            }
-    }
-    skypeResult.innerHTML = spliter.join(':');
-*/
-let resultSkype;
-for (let i=0; i < spliter.length; i++) {
-    if (spliter[i] == 'skype')
-        {
-            if (spliter[i+2] == 'call') 
-            {
-                //spliter[i+1] = 'X';
-                resultSkype = skypeValue.split(`${spliter[i+1]}`).join('X');
-                console.log(skypeValue.split(`${spliter[i+1]}`).join('X'));
-            }
-            else if (spliter[i+2] == 'chat') 
-            {
-                //spliter[i+1] = '*';
-                resultSkype = skypeValue.split(`${spliter[i+1]}`).join('*');
-                console.log(skypeValue.split(`${spliter[i+1]}`).join('*'));
-            }
-            //else spliter[i+1] = '#';
-            else 
-            {
-                resultSkype = skypeValue.split(`${spliter[i+1]}`).join('#');
-                console.log(skypeValue.split(`${spliter[i+1]}`).join('#'));
-            }
-        } 
-        /*if (spliter[i] != 'skype') 
-        {
+                //else spliter[i+1] = '#';
+                else 
+                {
+                    resultSkype = skypeValue.split(`${spliter[i+1]}`).join('#');
+                    console.log(skypeValue.split(`${spliter[i+1]}`).join('#'));
+                }
+            } 
+        if (resultSkype == undefined) {
             resultSkype = skypeValue;
-        }*/
-
-        skypeResult.innerHTML = resultSkype;
-        
-
+        }
+        skypeResult.innerText = resultSkype;       
     }
 
 }
 skypeBtn.onclick = skypeAnonymizer;
 
+//  Phone number anonymizer
+function phoneAnonymizer() { 
+    let phoneValue = document.getElementById("phoneValue").value,
+    phoneResult = document.getElementById("phoneResult");
+    let resultPhone;
 
-
-    //  skypeValue.includes('skype');
-
-    /*  let spliter = skypeValue.split(/:|,/);  // Result: (2) ["skype", "lorem"]
-        spliter[1] = '#';       // spliter[1]="lorem" -> spliter[1]="#"
-        spliter.join(':');      // Result: "skype:#"
-    */
-
-  /* if (skypeValue == '') {
-    skypeResult.innerHTML = '';
-   } else {
-        let spliter = skypeValue.split(/:|,/);  // Result: (2) ["skype", "lorem"]
-        spliter[1] = '#';       // spliter[1]="lorem" -> spliter[1]="#"      
-        skypeResult.innerHTML = spliter.join(':');       // Result: "skype:#"
-   }*/
-
-   //skypeResult.innerHTML = skypeValue.replace(/\s#\*.*/, '#');
+    if (resultPhone == undefined) {
+        resultPhone = phoneValue;
+    }
+    phoneResult.innerText = resultPhone; 
+}
+phoneBtn.onclick = phoneAnonymizer;
