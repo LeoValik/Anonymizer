8284d2d55f0fb903da4e67f473d5c8f403acf6b5
LeoValik <troval12989@gmail.com>
2019-05-18 17:35:26 +0300

    Anonymizer Skype: more closer to right way

--- index.js
+++ index.js
@@ -15,6 +15,66 @@ function skypeAnonymizer() {
     let skypeValue = document.getElementById("skypeValue").value,
     skypeResult = document.getElementById("skypeResult");
 
+
+    //let spliter = skypeValue.split(/:|,/);
+    let spliter = skypeValue.match(/\w+/g);
+    console.log(skypeValue);
+    console.log(spliter);
+/*
+    for (let i=0; i < spliter.length; i++) {
+        if (spliter[i] == 'skype')
+            {
+                if (spliter[i+2] == 'call') 
+                {
+                    spliter[i+1] = 'X';
+                }
+                else if (spliter[i+2] == 'chat') 
+                {
+                    spliter[i+1] = '*';
+                }
+                else spliter[i+1] = '#';
+            }
+    }
+    skypeResult.innerHTML = spliter.join(':');
+*/
+let resultSkype;
+for (let i=0; i < spliter.length; i++) {
+    if (spliter[i] == 'skype')
+        {
+            if (spliter[i+2] == 'call') 
+            {
+                //spliter[i+1] = 'X';
+                resultSkype = skypeValue.split(`${spliter[i+1]}`).join('X');
+                console.log(skypeValue.split(`${spliter[i+1]}`).join('X'));
+            }
+            else if (spliter[i+2] == 'chat') 
+            {
+                //spliter[i+1] = '*';
+                resultSkype = skypeValue.split(`${spliter[i+1]}`).join('*');
+                console.log(skypeValue.split(`${spliter[i+1]}`).join('*'));
+            }
+            //else spliter[i+1] = '#';
+            else 
+            {
+                resultSkype = skypeValue.split(`${spliter[i+1]}`).join('#');
+                console.log(skypeValue.split(`${spliter[i+1]}`).join('#'));
+            }
+        } 
+        /*if (spliter[i] != 'skype') 
+        {
+            resultSkype = skypeValue;
+        }*/
+
+        skypeResult.innerHTML = resultSkype;
+        
+
+    }
+
+}
+skypeBtn.onclick = skypeAnonymizer;
+
+
+
     //  skypeValue.includes('skype');
 
     /*  let spliter = skypeValue.split(/:|,/);  // Result: (2) ["skype", "lorem"]
@@ -22,9 +82,12 @@ function skypeAnonymizer() {
         spliter.join(':');      // Result: "skype:#"
     */
 
-   let spliter = skypeValue.split(/:|,/);  // Result: (2) ["skype", "lorem"]
-   spliter[1] = '#';       // spliter[1]="lorem" -> spliter[1]="#"      
-   skypeResult.innerHTML = spliter.join(':');       // Result: "skype:#"
+  /* if (skypeValue == '') {
+    skypeResult.innerHTML = '';
+   } else {
+        let spliter = skypeValue.split(/:|,/);  // Result: (2) ["skype", "lorem"]
+        spliter[1] = '#';       // spliter[1]="lorem" -> spliter[1]="#"      
+        skypeResult.innerHTML = spliter.join(':');       // Result: "skype:#"
+   }*/
 
-}
-skypeBtn.onclick = skypeAnonymizer;
+   //skypeResult.innerHTML = skypeValue.replace(/\s#\*.*/, '#');
